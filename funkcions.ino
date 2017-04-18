06b3feb6a56c9994e17ab2f2a6d5895fa9775572
StanislavJochman <s.jochman@gmail.com>
2017-04-18 14:42:29 +0200

    basic funkcionality
    basic funkcionality code for ATTEMP

---
+++ funkcions.ino
@@ -0,0 +1,182 @@
+int stovkyF(int temperature)
+  {
+    int stovkyF;
+    stovkyF = (temperature - jednotky(temperature))/100;
+    return stovkyF;
+  }
+int desiatky(int temperature)
+  {
+    int desiatky;
+    desiatky = (temperature - jednotky(temperature))/10;
+    return desiatky;
+  }
+int desiatkyF(int temperature)
+  {
+    int desiatkyF;
+    desiatkyF = (temperature/10)%10;
+    return desiatkyF;
+  }
+int jednotky(int temperature)
+  {
+    int jednotky;
+    jednotky = temperature%10;
+    return jednotky;
+  }
+int jednotkyF(int temperature)
+  {
+    int jednotkyF;
+    jednotkyF = (temperature - (stovkyF(temperature))*100)-(desiatkyF(temperature)*10);
+    return jednotkyF;
+  }
+int desatiny(float temperature)
+  {
+     int desatiny;
+     int teplota;
+     teplota = (temperature*10);
+     desatiny = teplota - (desiatky(temperature)*100) - (jednotky(temperature)*10);
+     return desatiny;
+    
+  }
+void tempC(float temperature)
+{
+  if (temperature>0 && temperature<86 )
+  { 
+    cislo(desiatky(temperature));
+    delay(500);
+    cislo(10);
+    delay(200);
+    cislo(jednotky(temperature));
+    delay(500);
+    cislo(14);
+    delay(500);
+    cislo(desatiny(temperature));
+    delay(500);
+    cislo(11);
+    delay(500);
+
+  }
+  else if(temperature==0)
+  {
+    cislo(0);
+    delay(500);
+    cislo(10);
+    delay(500);
+    cislo(11);
+    delay(500);
+    cislo(10);
+    delay(500);
+    cislo(10);
+  }
+  else if (temperature>-100 && temperature<0)
+  {
+    
+    
+  }
+  else
+  {
+    cislo(12);
+    delay(500);
+    cislo(10);
+    delay(500);
+  }
+}
+
+void tempF(float temperature)
+{
+  if (temperature>0 && temperature<186 )
+  { 
+    if (temperature>99){
+      cislo(stovkyF(temperature));
+      delay(500);
+      cislo(10);
+      delay(200);
+      cislo(desiatkyF(temperature));
+      delay(500);
+      cislo(10);
+      delay(200);
+      cislo(jednotkyF(temperature));
+      delay(500);
+      cislo(15);
+      delay(500);
+    }
+    else
+    {
+      cislo(desiatky(temperature));
+      delay(500);
+      cislo(10);
+      delay(200);
+      cislo(jednotky(temperature));
+      delay(500);
+      cislo(15);
+      delay(500);
+    }
+
+  }
+  else if(temperature==0)
+  {
+    cislo(0);
+    delay(500);
+    cislo(10);
+    delay(500);
+    cislo(11);
+    delay(500);
+    cislo(10);
+  }
+  else
+  {
+    cislo(12);
+    delay(500);
+    cislo(10);
+    delay(500);
+  }
+}
+void cislo(int x)
+{
+
+digitalWrite(latch, LOW);
+for (int i = 7; i>=0; i--)
+{
+digitalWrite(clock, LOW);
+digitalWrite(data, znaky[x][i] );
+digitalWrite(clock, HIGH);
+}
+digitalWrite(latch, HIGH);
+}
+int BTN()
+{ 
+  int state = digitalRead(button);
+  delay(100);
+  return state;
+}
+void menu()
+{ 
+  state = 11;
+  while (1==1)
+  {
+    if(counter>21){break;}
+    else
+    {
+      cislo(state);
+      
+      if (BTN()==0)
+      {
+        
+         counter+=1;
+         if (state==11)
+           {
+              state=15;
+           }
+         else if (state=15)
+           {
+              state=11;
+           }
+         delay(50);
+        
+      }
+      else
+      {
+        counter = 0;
+      }
+    }
+  }
+}
---
+++ segment_attiny.ino
@@ -0,0 +1,72 @@
+#include <DallasTemperature.h>
+#include <OneWire.h>
+#define ONE_WIRE_BUS 4
+
+int data = 0;//pin 14
+int latch = 2;
+int clock = 1;
+int button = A3;
+int state;
+int counter;
+OneWire oneWire(ONE_WIRE_BUS); 
+DallasTemperature sensors(&oneWire);
+float temperature;
+
+int znaky [16][8]{
+{0,1,1,1,0,1,1,1},//0
+{0,0,0,1,0,1,0,0},//1
+{1,0,1,1,0,0,1,1},//2
+{1,0,1,1,0,1,0,1},//3
+{1,1,0,1,0,1,0,0},//4
+{1,1,1,0,0,1,0,1},//5
+{1,1,1,0,0,1,1,1},//6
+{0,0,1,1,0,1,0,0},//7
+{1,1,1,1,0,1,1,1},//8
+{1,1,1,1,0,1,0,0},//9
+//-------------------
+{0,0,0,0,0,0,0,0},//clear
+{0,1,1,0,0,0,1,1},//celzia
+{1,1,1,0,0,0,1,1},//error
+{1,0,0,0,0,0,0,0}, // -
+{0,0,0,0,1,0,0,0}, //dot
+{1,1,1,0,0,0,1,0}//fahrenheit
+};
+
+
+
+void setup()
+{
+
+pinMode(data,OUTPUT);
+pinMode(clock,OUTPUT);
+pinMode(latch,OUTPUT);
+pinMode(button,INPUT_PULLUP);
+sensors.begin(); 
+sensors.requestTemperatures();
+delay(500);
+menu();
+}
+void loop()
+{
+ 
+  if(BTN()==0)
+  { 
+    delay(100);
+    sensors.requestTemperatures();
+    if (state == 15)
+    {
+      temperature = round(sensors.getTempFByIndex(0));
+      delay(100);
+      tempF(temperature);
+    }
+    else if (state == 11)
+    {
+      temperature = sensors.getTempCByIndex(0);
+      delay(100);
+      tempC(temperature);
+    }
+  }
+  else{cislo(10);}
+  
+}
+
